42ef994a3e9da8c7a2f558627f99c7545245b061
Francisco de Melo <franciscomelojr@gmail.com>
2020-04-20 02:55:25 -0400

    StandaloneJNDI: shows and adds context properties. StandaloneFile reads jndi from file. jndi.properties is the file.

--- Bean/StandaloneJNDI.java
+++ Bean/StandaloneJNDI.java
@@ -6,18 +6,23 @@ import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import java.util.Hashtable;
 
+import java.lang.reflect.Field;
+
+import java.util.Properties;
+
+import java.io.*;
+
 public class StandaloneJNDI {
-  public static void  main(String [] rgstring) {
+  //Main
+  public static void  main(String [] rgstring){
     try {
       // Create the initial context.  The environment
       // information specifies the JNDI provider to use
       // and the initial URL to use (in our case, a
       // directory in URL form -- file:///...).
-      Hashtable hashtableEnvironment = new Hashtable();
-      hashtableEnvironment.put( Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-      hashtableEnvironment.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
-      Context context = new InitialContext(hashtableEnvironment);
 
+      Context context = getContext();
+      //Add and Get environment
       addEnvironment(context);
       getEnvironment(context);
       // If you provide no other command line arguments,
@@ -27,18 +32,31 @@ public class StandaloneJNDI {
         NamingEnumeration namingenumeration = context.listBindings("");
         while (namingenumeration.hasMore()) {
           Binding binding = (Binding)namingenumeration.next();
-          System.out.println(binding.getName() + " XXX " + binding.getObject()
+          System.out.println(binding.getName() + " === " + binding.getObject()
           );
         }
 
 		NameClassPair ncp = listNames(context);
   		//context.unbind(ncp.getName());    
+    listFields(ncp);
     }
     catch (NamingException namingexception) {
-      namingexception.printStackTrace();
+         System.out.println("Name not here");
+    }
+    catch (NoSuchFieldException nosuchfield) {
+         System.out.println("Field not here");
     }
   }
 
+  //Create Context hard coded:
+  public static Context getContext() throws NamingException{
+      Hashtable hashtableEnvironment = new Hashtable();
+      hashtableEnvironment.put( Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+      hashtableEnvironment.put(Context.PROVIDER_URL,"remote+http://localhost:8180");//(Context.PROVIDER_URL,"remote+http://localhost:8080");
+      Context context = new InitialContext(hashtableEnvironment);
+      return context;
+
+  }
   //Get Environment:
   public static void getEnvironment(Context context) throws NamingException{
   	System.out.println(context.getEnvironment());
@@ -49,7 +67,7 @@ public class StandaloneJNDI {
   	context.addToEnvironment("com.wiz.jndi.wizProp", "wizards");
   }
 
-  //Get list:
+  //Get list Names
   public static NameClassPair listNames(Context context) throws NamingException{
 
         NamingEnumeration ctxlst =   context.list("");  
@@ -59,5 +77,18 @@ public class StandaloneJNDI {
             System.out.println( ncp.getClassName() + " : " + ncp.isRelative() +" : " + ncp.getName()  +" : " + ncp.toString() + " : " + ncp.getNameInNamespace());        
         }  
   		return ncp;
+  }
+
+  //Get field objects form object
+  public static void listFields(Object unk) throws NoSuchFieldException {
+    try{
+        Class<?> clazz = unk.getClass();
+        System.out.println(unk.getClass().toString());
+        Field field = clazz.getField("fieldName"); //Note, this can throw an exception if the field doesn't exist.
+        Object fieldValue = field.get(unk);
+    }catch(IllegalAccessException illegalaccess){
+      illegalaccess.printStackTrace();
+    }
+
   }
 }
