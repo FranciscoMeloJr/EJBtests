31b8b52263cfabe35431ba4fb7b2761660bfe578
Francisco de Melo <franciscomelojr@gmail.com>
2020-04-21 01:32:01 -0400

    EJB for secure test - dont forget the jars

---
+++ BeanSecurity/SecureSum.java
@@ -0,0 +1,15 @@
+import javax.ejb.Stateless;
+import javax.annotation.security.RolesAllowed;
+
+import org.jboss.annotation.security.SecurityDomain;
+
+
+@Stateless
+@RolesAllowed({ "guest" }) //Added
+@SecurityDomain("other") //Added
+public class SecureSum implements SecureSumRemote 
+{
+	public int add(int a, int b){
+		return a+b;
+	}
+}
---
+++ BeanSecurity/SecureSumRemote.java
@@ -0,0 +1,8 @@
+import javax.ejb.Remote;
+//Someone based on @author <a href="mailto:sguilhen@redhat.com">Stefan Guilhen</a>
+
+@Remote
+public interface SecureSumRemote 
+{
+	int add(int a, int b);
+}
