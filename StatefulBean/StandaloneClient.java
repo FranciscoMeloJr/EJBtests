7e6db26a3fab84208f660bb3be76036d31d02945
Francisco de Melo <franciscomelojr@gmail.com>
2020-04-23 23:19:44 -0400

    adding the jndi.properties

--- StatefulBean/StandaloneClient.java
+++ StatefulBean/StandaloneClient.java
@@ -1,19 +1,28 @@
 import javax.naming.Context;
 import javax.naming.InitialContext;
+
 import javax.naming.NamingException;
+import java.io.IOException;
 
 import java.util.Properties;
 
 import java.util.*;
+
+import java.io.FileInputStream;
 
 public class StandaloneClient {
 
 	public static void main(String[]args) throws NamingException {
    	
     System.out.println("Getting the Remote");
+	LibraryStatefulSessionBeanRemote libraryBean = null;
 
-    LibraryStatefulSessionBeanRemote libraryBean = (LibraryStatefulSessionBeanRemote) getInitialContext().lookup("ejb:/EjbStateful/LibraryStatefulSessionBean!LibraryStatefulSessionBeanRemote?stateful");
+    try{
+    	libraryBean = (LibraryStatefulSessionBeanRemote) getInitialContext().lookup("ejb:/EjbStateful/LibraryStatefulSessionBean!LibraryStatefulSessionBeanRemote?stateful");
     //Results: Got the remote InterfaceProxy for remote EJB StatelessEJBLocator for "/EjbRemote/Sum", view is interface SumRemote, affinity is None
+    }catch (Exception e){
+        System.out.println("Something went wrong ");
+    }
 
     System.out.println("Got the remote Interface" + libraryBean);
     libraryBean.addBook("Florence");
@@ -23,10 +32,9 @@ public class StandaloneClient {
     	}
 	}
 	//getInitialContext:
-	public static Context getInitialContext() throws NamingException{
+	public static Context getInitialContext() throws NamingException, IOException{
 	   Properties props = new Properties();
-	   props.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-	   props.put(Context.PROVIDER_URL,"remote+http://localhost:8080");//"remote+http://localhost:8080,remote+http://localhost:8180");
+	   props.load(new FileInputStream("jndi.properties"));
 	   final Context context = new InitialContext(props);
 	   return context;
 	}
