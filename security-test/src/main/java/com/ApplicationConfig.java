0b00381adc0f38f6530b523b8d7f26b5340ced19
Francisco de Melo <franciscomelojr@gmail.com>
2020-04-25 03:04:56 -0400

    ldap config file

--- security-test/src/main/java/com/ApplicationConfig.java
+++ security-test/src/main/java/com/ApplicationConfig.java
@@ -6,15 +6,23 @@ import javax.security.enterprise.authentication.mechanism.http.FormAuthenticatio
 import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
 import javax.security.enterprise.identitystore.LdapIdentityStoreDefinition;
 
+//Form based authentication
 @FormAuthenticationMechanismDefinition(
+	//Login with an index page and a error page.
     loginToContinue = @LoginToContinue(loginPage = "/index.xhtml", errorPage = "/error.xhtml")
 )
+
 @LdapIdentityStoreDefinition(
+	//URL where the LDAP server can be reached.
     url = "ldap://localhost:10389",
+    //Search base for looking up callers (e.g., "ou=caller,dc=jsr375,dc=net"). 
     callerSearchBase = "ou=Users,dc=example,dc=com",
+    //Search base for looking up groups (e.g., "ou=group,dc=jsr375,dc=net"). 
     groupSearchBase = "ou=Groups,dc=example,dc=com",
+    //Search filter to find groups when groupSearchBase is set. The search is performed starting from the groupSearchBase DN with the scope specified by groupSearchScope.
     groupSearchFilter = "(&(member=%s)(objectClass=groupOfNames))"
 )
+//Faces Config:
 @FacesConfig
 @ApplicationScoped
 public class ApplicationConfig {
